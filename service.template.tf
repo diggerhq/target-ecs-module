847f8b2e646dc1a2974e05fc1c51b8925ef3d8f9
Dias Saparov <dias@digger.dev>
2022-11-02 11:22:12 +0000

    render environmental variables

--- service.template.tf
+++ service.template.tf
@@ -25,7 +25,7 @@ module "monitoring" {
     subnet_ids = var.public_subnets
     vpcCIDRblock = var.vpcCIDRblock
     {% if environment_variables %}
-    environment_variables = jsondecode({{ environment_variables }})
+    environment_variables = jsondecode({{ environment_variables | tojson }})
     {% endif %}
     {%- if internal is defined %}
     internal={{ internal }}
@@ -68,7 +68,7 @@ module "monitoring" {
     internal={{ internal }}
     {%- endif %}
     {% if environment_variables %}
-    environment_variables = jsondecode({{ environment_variables }})
+    environment_variables = jsondecode({{ environment_variables | tojson }})
     {% endif %}
 
     alb_internal = false
@@ -190,7 +190,7 @@ module "monitoring" {
       ecs_autoscale_max_instances = "{{ecs_autoscale_max_instances}}"
     {% endif %}
     {% if environment_variables %}
-    environment_variables = jsondecode({{ environment_variables }})
+    environment_variables = jsondecode({{ environment_variables | tojson }})
     {% endif %}
   }
 
