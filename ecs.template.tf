60e8f5427bf3f86d2b296f4ac7dfa82d42359890
alexey <alexey@digger.dev>
2022-11-18 16:32:48 +0000

    update ecr repo url

--- ecs.template.tf
+++ ecs.template.tf
@@ -36,7 +36,7 @@ resource "aws_ecs_task_definition" "app" {
   task_role_arn = aws_iam_role.ecs_task_role.arn
   container_definitions = jsonencode([{
   name      = var.ecs_service_name
-  image     = aws_ecr_repository.ecr_repo.repository_url
+  image     = "${aws_ecr_repository.ecr_repo.repository_url}:latest"
   essential = true
   portMappings = [{
     protocol      = "tcp"
