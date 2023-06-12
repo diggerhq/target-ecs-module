0ef81faa2d9c713f4b4d89fc4124cc5f4fc22387
Alexey Skriptsov <alexey@digger.dev>
2023-06-12 16:02:24 +0100

    add aws_s3_bucket_ownership_controls

--- lb.template.tf
+++ lb.template.tf
@@ -52,7 +52,16 @@ resource "aws_s3_bucket" "lb_access_logs" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket_ownership_controls" "lb_access_logs_ownership_controls" {
+  bucket = aws_s3_bucket.lb_access_logs.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_acl" "lb_access_logs_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.lb_access_logs_ownership_controls]
+
   bucket = aws_s3_bucket.lb_access_logs.id
   acl    = "private"
 }
